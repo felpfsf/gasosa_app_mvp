f53b049294f86215713635647d6a0486b0777568
Felipe Ferreira <feferreira.sf@gmail.com>
2025-08-21 01:02:20 -0300

    🔧 Update GasosaApp to use AppTheme.dark and remove custom theme builder

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gasosa_app/presentation/routes/app_router.dart';
+import 'package:gasosa_app/theme/app_theme.dart';
 
 class GasosaApp extends StatelessWidget {
   const GasosaApp({super.key});
@@ -8,21 +9,9 @@ class GasosaApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Gasosa',
-      theme: _buildTheme(),
+      theme: AppTheme.dark,
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-    );
-  }
-
-  ThemeData _buildTheme() {
-    final base = ThemeData(
-      colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF6C5CE7)),
-      useMaterial3: true,
-    );
-
-    return base.copyWith(
-      appBarTheme: base.appBarTheme.copyWith(centerTitle: true),
-      visualDensity: VisualDensity.adaptivePlatformDensity,
     );
   }
 }

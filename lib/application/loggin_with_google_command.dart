1484fb92173bb77198b2fe6504bc4b32cbb48578
Felipe Ferreira <feferreira.sf@gmail.com>
2025-08-23 11:21:04 -0300

    🔧 Refactor LoginWithGoogleCommand to use AuthService instead of AuthRepository for Google login functionality

--- lib/application/loggin_with_google_command.dart
+++ lib/application/loggin_with_google_command.dart
@@ -1,8 +1,11 @@
-import 'package:gasosa_app/domain/auth/auth_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:gasosa_app/core/errors/failure.dart';
+import 'package:gasosa_app/domain/services/auth_service.dart';
 
 class LoginWithGoogleCommand {
-  const LoginWithGoogleCommand(this._repo);
-  final AuthRepository _repo;
+  LoginWithGoogleCommand(this._auth);
 
-  Future<void> call() => _repo.loginWithGoogle();
+  final AuthService _auth;
+
+  Future<Either<Failure, AuthUser>> call() => _auth.loginWithGoogle();
 }

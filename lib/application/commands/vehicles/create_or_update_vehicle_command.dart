78b119d373592fc8a900ade782868e959314339a
Felipe Ferreira <feferreira.sf@gmail.com>
2025-08-26 15:15:04 -0300

    🔧 Add CreateOrUpdateVehicleCommand, DeleteVehicleCommand, and LoadVehiclesCommand for vehicle management operations

---
+++ lib/application/commands/vehicles/create_or_update_vehicle_command.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:gasosa_app/core/errors/failure.dart';
+import 'package:gasosa_app/domain/entities/vehicle.dart';
+import 'package:gasosa_app/domain/repositories/vehicle_repository.dart';
+
+class CreateOrUpdateVehicleCommand {
+  CreateOrUpdateVehicleCommand(this._repository);
+
+  final VehicleRepository _repository;
+
+  Future<Either<Failure, Unit>> call(VehicleEntity entity) async {
+    return entity.id.isEmpty ? _repository.createVehicle(entity) : _repository.updateVehicle(entity);
+  }
+}
---
+++ lib/application/commands/vehicles/delete_vehicle_command.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:gasosa_app/core/errors/failure.dart';
+import 'package:gasosa_app/domain/repositories/vehicle_repository.dart';
+
+class DeleteVehicleCommand {
+  DeleteVehicleCommand(this._repository);
+
+  final VehicleRepository _repository;
+
+  Future<Either<Failure, Unit>> call(String vehicleId) async => _repository.deleteVehicle(vehicleId);
+}

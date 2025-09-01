459b936113a61391484399a03ea46d85364d8abf
Felipe Ferreira <feferreira.sf@gmail.com>
2025-09-01 15:57:37 -0300

    🔧 Remove unused vehicle and receipt photo command files and update locator to use generic photo commands

--- lib/application/commands/vehicles/vehicle_photo_command.dart
+++
@@ -1,5 +0,0 @@
-import 'package:gasosa_app/application/commands/photos/delete_photo_command.dart';
-import 'package:gasosa_app/application/commands/photos/save_photo_command.dart';
-
-typedef SaveVehiclePhotoCommand = SavePhotoCommand;
-typedef DeleteVehiclePhotoCommand = DeletePhotoCommand;

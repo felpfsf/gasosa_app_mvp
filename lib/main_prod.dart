976afb6c92fbc585aafde545fc2fb0fc22f10bf7
Felipe Ferreira <feferreira.sf@gmail.com>
2025-09-02 00:40:50 -0300

    🔧 Add localization support for Brazilian Portuguese in app and main files

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -2,12 +2,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gasosa_app/firebase_options_dev.dart';
 import 'package:gasosa_app/flavor.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  await initializeDateFormatting('pt_BR');
 
   Flavor.instance = const Flavor(
     name: 'prod',
--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:gasosa_app/core/di/locator.dart';
 import 'package:gasosa_app/core/viewmodel/loading_controller.dart';
 import 'package:gasosa_app/presentation/routes/app_router.dart';
@@ -16,6 +17,14 @@ class GasosaApp extends StatelessWidget {
       theme: AppTheme.dark,
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('pt', 'BR'),
+      ],
       builder: (context, child) => GlobalLoaderOverlay(
         controller: loading,
         child: child,

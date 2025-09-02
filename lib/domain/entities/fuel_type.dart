1cab79663cfd6b3cdfb07e55a95e6632b3f7e346
Felipe Ferreira <feferreira.sf@gmail.com>
2025-09-02 18:32:49 -0300

    🔧 Add FuelType extension with display names for improved fuel type representation

--- lib/domain/entities/fuel_type.dart
+++ lib/domain/entities/fuel_type.dart
@@ -1,1 +1,13 @@
 enum FuelType { gasoline, ethanol, diesel, flex, gnv }
+
+extension FuelTypeExtension on FuelType {
+  static const Map<FuelType, String> _displayNames = {
+    FuelType.gasoline: 'Gasolina',
+    FuelType.ethanol: 'Etanol',
+    FuelType.diesel: 'Diesel',
+    FuelType.flex: 'Flex',
+    FuelType.gnv: 'GNV',
+  };
+
+  String get displayName => _displayNames[this] ?? name;
+}

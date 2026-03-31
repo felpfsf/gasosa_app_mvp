423ebb67a77a02b1fb94434217798699450f8c94
Felipe Ferreira <feferreira.sf@gmail.com>
2026-03-31 11:36:21 -0300

    Squashed commit of the following:
    commit eb2b314aae982e210808cfe03d86c4eb28af2d6d
    Author: Felipe Ferreira <feferreira.sf@gmail.com>
    Date:   Tue Mar 31 11:29:48 2026 -0300
    
        chore: update Firebase URL scheme in Info.plist
    
    commit 1e51428b8a1fc53455f9016dac4cda25bdfd4540
    Author: Felipe Ferreira <feferreira.sf@gmail.com>
    Date:   Tue Mar 31 11:21:01 2026 -0300
    
        chore: update Firebase configuration and add Crashlytics upload script
    
    commit b6fb37d97a3b8896d04da336f6b33718ca358dfd
    Author: Felipe Ferreira <feferreira.sf@gmail.com>
    Date:   Tue Mar 31 11:12:15 2026 -0300
    
        chore: update Firebase configuration and package identifiers

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     // START: FlutterFire Configuration
     id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
     // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
@@ -9,7 +10,7 @@ plugins {
 }
 
 android {
-    namespace = "dev.etherealcodes.gasosa_app.gasosa_app"
+    namespace = "etherealarc.tech.gasosapp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +25,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "dev.etherealcodes.gasosa_app.gasosa_app"
+        applicationId = "etherealarc.tech.gasosapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
